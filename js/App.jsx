958dd31f522d05628aef2fc478fa9c1304e1ffc3
Steven Straatemans <steven@straatemans.nl>
2017-09-26 21:35:19 +0200

    hello world

---
+++ js/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <span>hello world!</span>
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,55 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 const config = {
   context: __dirname,
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './js/ClientApp.jsx'
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./js/ClientApp.jsx",
   ],
-  devtool: 'cheap-eval-source-map',
+  devtool: "cheap-eval-source-map",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/public/",
   },
   devServer: {
     hot: true,
-    publicPath: '/public/',
-    historyApiFallback: true
+    publicPath: "/public/",
+    historyApiFallback: true,
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
+    extensions: [".js", ".jsx", ".json"],
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
   module: {
     rules: [
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.jsx?$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
+        loader: "eslint-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: [path.resolve('js'), path.resolve('node_modules/preact-compat/src')]
-      }
-    ]
-  }
+        loader: "babel-loader",
+        include: [
+          path.resolve("js"),
+          path.resolve("node_modules/preact-compat/src"),
+        ],
+      },
+    ],
+  },
 };
-
-console.log(process.env.NODE_ENV);
-
-if (process.env.NODE_ENV === 'production') {
-  config.entry = './js/ClientApp.jsx';
-  config.devtool = false;
-  config.plugins = [];
-}
 
 module.exports = config;
